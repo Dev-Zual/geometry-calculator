519db82556316ec8cbdeeb11d48341dd8ad6bb79
Zual Rana <freelancerzual@gmail.com>
2024-12-22 21:21:08 +0600

    code reusable

--- js/calculate.js
+++ js/calculate.js
@@ -1,14 +1,23 @@
 document.getElementById("triangle-btn").addEventListener("click", function () {
-  const bInput = document.getElementById("b-input");
-  const hInput = document.getElementById("h-input");
-  const baseValue = bInput.value;
-  const heightValue = hInput.value;
-  const p = document.createElement("p");
-  p.innerText = `Triangle Area = ${
-    0.5 * Number(baseValue) * Number(heightValue)
-  }`;
-  const resultContainer = document.getElementById("result-container");
-  resultContainer.appendChild(p);
+  const baseValue = getInputValue("b-input");
+  const heightValue = getInputValue("h-input");
+
+  const value = 0.5 * baseValue * heightValue;
+
+  displayValue("result-container", value);
+
   bInput.value = "";
   hInput.value = "";
 });
+
+function getInputValue(input) {
+  const getInput = document.getElementById(input).value;
+  const inputValue = Number(getInput);
+  return inputValue;
+}
+function displayValue(input, value) {
+  const p = document.createElement("p");
+  p.innerText = `Triangle Area = ${value}`;
+  const resultContainer = document.getElementById(input);
+  resultContainer.appendChild(p);
+}
